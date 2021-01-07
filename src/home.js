096ad06fdfcb3cd7ddbaefe822e5b940be78d88a
Asim26 <asimmehmood807@gmail.com>
2021-01-07 17:29:34 +0500

    c1

---
+++ src/home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Asim</h2>
+            </div>
+        )
+    }
+}
